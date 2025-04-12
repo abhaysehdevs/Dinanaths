b48fb865e8a90d87b86762060a8f3ae289b0cfcc
abhaysehdevs <abhaysehdevofficial@gmail.com>
2025-04-12 23:03:22 +0530

    uploadmain

---
+++ products.js
@@ -0,0 +1,182 @@
+const products = [
+  {
+    id: 1,
+    name: "Suhaga",
+    price: 150,
+    description: "A stylish and comfortable sofa.",
+    images: ["images/Suhagabig.png", "images/Suhaga small.jpg"],
+    videos: [], // Product videos
+    category: "Chemicals",
+    stock: 10, // Stock availability
+    variants: {
+      Material: ["Cotton", "Silk"],
+    Pattern: ["Striped", "Plain"]
+    }
+  },
+  {
+    id: 2,
+    name: "sharpning stone",
+    price: 3500,
+    description: "A sleek and modern floor lamp.",
+    images: ["images/sharping stone1.png", "images/sharping stone2.png", "images/sharping stone3.png"],
+    videos: [],
+    category: "Abrasives",
+    stock: 0 // Out of stock
+  },
+  {
+    id: 3,
+    name: "Tik-tak Citric Acid",
+    price: 3500,
+    description: "A sleek and modern floor lamp.",
+    images: ["images/citric acid.jpg", "images/citric acid.png"],
+    videos: [],
+    category: "Abrasives",
+    stock: 10 // Out of stock
+  },
+  {
+    id: 4,
+    name: "Tik-tak boric acid",
+    price: 3500,
+    description: "A sleek and modern floor lamp.",
+    images: ["images/boric acid.jpg", "images/boric acid2.png", "images/boric acid3.png"],
+    videos: [],
+    category: "Abrasives",
+    stock: 10 // Out of stock
+  },
+  {
+    id: 5,
+    name: "Clarion Saw Blade",
+    price: 3500,
+    description: "A sleek and modern floor lamp.",
+    images: ["images/clarion blade.jpg", "images/clarionsawblade.jpg", "images/clarionsawblade2.jpg"],
+    videos: [],
+    category: "Abrasives",
+    stock: 10 // Out of stock
+  },
+  {
+    id: 6,
+    name: "Diamond Tester",
+    price: 3500,
+    description: "A sleek and modern floor lamp.",
+    images: ["images/Diamond tester.jpg", "images/Diamondtester.jpg"],
+    videos: [],
+    category: "Abrasives",
+    stock: 10 // Out of stock
+  },
+  {
+    id: 7,
+    name: "Gem Box",
+    price: 3500,
+    description: "A sleek and modern floor lamp.",
+    images: ["images/gem box.jpg", "images/gem box2.jpg", "images/gem box3.jpg"],
+    videos: [],
+    category: "Abrasives",
+    stock: 10 // Out of stock
+  },
+  {
+    id: 8,
+    name: "Bangle Sizer",
+    price: 3500,
+    description: "A sleek and modern floor lamp.",
+    images: ["images/Banglesizer.jpg"],
+    videos: [],
+    category: "Abrasives",
+    stock: 10 // Out of stock
+  },
+  {
+    id: 9,
+    name: "Ear Piercing Gun",
+    price: 3500,
+    description: "Dinanath's brand original ear piercing gun used for piercing through studs in ear with full safety",
+    images: ["images/ear gun.jpg"],
+    videos: [],
+    category: "Abrasives",
+    stock: 10 // Out of stock
+  },
+  {
+    id: 10,
+    name: "Ring Adjusting Machine",
+    price: 3500,
+    description: "Ring a adjusting machine used for adjustment of rings and other jewelry items. It is used for expansions and contraction of the ring according to the finger size, avoiding cracks and other kind of damage on the ring.",
+    images: ["images/ring adjuster.jpg", "images/Picsart_23-05-22_12-46-32-547.png"],
+    videos: [],
+    category: "Abrasives",
+    stock: 10 // Out of stock
+  },
+  {
+    id: 11,
+    name: "Golden P.C",
+    price: 3500,
+    description: "Ring a adjusting machine used for adjustment of rings and other jewelry items. It is used for expansions and contraction of the ring according to the finger size, avoiding cracks and other kind of damage on the ring.",
+    images: ["images/pc.jpg", "images/pc2.png", "images/pc3.jpg"],
+    videos: [],
+    category: "Abrasives",
+    stock: 10 // Out of stock
+  },
+  {
+    id: 12,
+    name: "Jewellery Tags",
+    price: 3500,
+    description: "Ring a adjusting machine used for adjustment of rings and other jewelry items. It is used for expansions and contraction of the ring according to the finger size, avoiding cracks and other kind of damage on the ring.",
+    images: ["images/Tags.jpeg", "images/tags3.jpg", "images/tags7.jpg", "images/jewellery tags.jpg"],
+    videos: [],
+    category: "Abrasives",
+    stock: 10 // Out of stock
+  },
+  {
+    id: 13,
+    name: "Dinanath's Tweezers",
+    price: 3500,
+    description: "A sleek and modern floor lamp.",
+    images: ["images/Twezzers2.png", "images/Tweezers.jpeg", "images/twezzerbox.jpg", "images/Curved Self Lock Tweezers.png"],
+    videos: [],
+    category: "Abrasives",
+    stock: 10 // Out of stock
+  },
+  {
+    id: 14,
+    name: "Universal gas lighter refill",
+    price: 3500,
+    description: "A sleek and modern floor lamp.",
+    images: ["images/gasrefill.png"],
+    videos: [],
+    category: "Abrasives",
+    stock: 10 // Out of stock
+  },
+    {
+      id: 15,
+      name: "Dinanath's Lakh Bangle",
+      price: 3500,
+      description: "A sleek and modern floor lamp.",
+      images: ["images/lakh Bangle.jpg", "images/Lakh Bangle2.png"],
+      videos: [],
+      category: "Abrasives",
+      stock: 10 // Out of stock
+    },
+    {
+      id: 16,
+      name: "minicraft hand engraver",
+      price: 3500,
+      description: "A sleek and modern floor lamp.",
+      images: ["images/minicraft hand engraver.jpg"],
+      videos: [],
+      category: "Abrasives",
+      stock: 10 // Out of stock
+    },
+    {
+      id: 17,
+      name: "Graphite Crucible",
+      price: 3500,
+      description: "A sleek and modern floor lamp.",
+      images: ["images/graphite crucible.jpg"],
+      videos: [],
+      category: "Abrasives",
+      stock: 10 // Out of stock
+    },
+    {
+      id: 16,
+      name: "Graphite Crucible",
+      Description: " sample"
+      
+    }
+];
